d9f82075be53da3363cd6956876c7fccaa583fe6
Girraj Kushwah <tgirrajindia@gmail.com>
2024-09-28 19:56:14 +0530

    edited src/middleware/errorHandler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -4,5 +4,4 @@ export function errorHandler(err: Error, req: Request, res: Response, next: Next
     console.log(err);
     // console.error(err.stack); // This JS property returns string representing the location in the code where the error occurred.
     res.status(500).json({ error: 'Internal Server Error' });
-    next();
 }
